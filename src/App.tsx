f4ee47897cc3c133b3087e97e535e62afa2d94d9
JP <85654561+jp524@users.noreply.github.com>
2023-04-03 21:28:00 -0400

    Add option to have multiple Work and Education Components

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,30 @@ import React from 'react';
 import './App.css';
 import Education from './components/Education';
 import Work from './components/Work';
+import AppState from './interfaces/AppState';
 
-class App extends React.Component {
+class App extends React.Component<{}, AppState> {
+  constructor(props: any) {
+    super(props);
+
+    this.state = {
+      numWork: 1,
+    };
+  }
+
+  addWork = () => {
+    this.setState({
+      numWork: this.state.numWork + 1,
+    });
+  };
+
   render() {
+    const workComponents = [];
+
+    for (let i = 0; i < this.state.numWork; i += 1) {
+      workComponents.push(<Work key={i} />);
+    }
+
     return (
       <div className="app">
         <div className="section">
@@ -13,7 +34,8 @@ class App extends React.Component {
         </div>
         <div className="section">
           <h1>Work</h1>
-          <Work />
+          {workComponents}
+          <button onClick={this.addWork}>Add Work Experience</button>
         </div>
       </div>
     );
---
+++ src/interfaces/AppState.ts
@@ -0,0 +1,5 @@
+interface AppState {
+  numWork: number;
+}
+
+export default AppState;
