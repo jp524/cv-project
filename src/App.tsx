750771e4c84a375827736c9f33c28112a3b1f7e7
JP <85654561+jp524@users.noreply.github.com>
2023-04-09 17:03:39 -0400

    Set up Sass config and layout files

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './styles/App.scss';
 import General from './components/General';
 import Education from './components/Education';
 import Work from './components/Work';
@@ -136,7 +136,7 @@ class App extends React.Component<{}, AppState> {
     const { generalObject, workObjects, educationObjects } = this.state;
 
     return (
-      <div className="app">
+      <div className="container">
         <div className="section">
           <General
             generalObject={generalObject}
